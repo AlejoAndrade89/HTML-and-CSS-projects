d30c34ccfa4be9d0be91c845ef79ee7abd1b581d
Alejandro Andrade <91246000+AlejoAndrade89@users.noreply.github.com>
2024-04-25 13:24:06 -0500

    adding the Basic_HTML_and_JavaScript_1” folder
    Assignment added and completed

---
+++ Basic_HTML_and_Javascript_1/Basic_Javascript_1.js
@@ -0,0 +1,10 @@
+alert();
+<script>
+	funtion My_First_Function(){
+		var String = "Kiss me I'm Irish!";
+		var result = String.fontcolor("green");
+		document.getElementById("Irish").innerHTML =
+		result;
+	
+	}
+</script>
